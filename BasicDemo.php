972579bb532c816b19b7777e47b264d3613c49da
EL OUFIR Hatim <eloufirhatim@gmail.com>
2023-12-01 13:50:53 -0500

    Update BasicDemo.php

--- BasicDemo.php
+++ BasicDemo.php
@@ -15,6 +15,8 @@ class BasicDemo extends Kanban
 
     protected static bool $enableCreateAction = true;
 
+    public bool $showFilters = true;
+
     protected static ?string $slug = 'basic-demo';
 
     protected static ?string $title = 'Basic Demo';
@@ -22,6 +24,8 @@ class BasicDemo extends Kanban
     protected $listeners = [
         'filament-kanban.record-sorted' => 'recordSorted',
         'filament-kanban.record-dragged' => 'recordDragged',
+        'filament-kanban.filter' => 'filter',
+        'filament-kanban.reset-filter' => 'resetFilter',
     ];
 
     public function mount(): void
@@ -101,5 +105,64 @@ class BasicDemo extends Kanban
             ->title('Dragged')
             ->body('Record dragged successfully!')
             ->send();
+    }
+
+    public function filter(array $data): void
+    {
+        $records = collect(KanbanService::getRecords());
+        $this->records = $records->filter(function ($item) use ($data) {
+            // Deadline
+            $filterDeadline = !isset($data['deadline']);
+            if (($data['deadline'] ?? null) && ($item['deadline'] ?? null)) {
+                $itemDate = $this->deadlineDate($item['deadline']);
+                $date = $this->deadlineDate($data['deadline']);
+                $filterDeadline = $itemDate->gte($date);
+            }
+            if (!$filterDeadline) return false;
+
+            // Title
+            $filterTitle = !isset($data['title']);
+            if (($data['title'] ?? null)) {
+                $filterTitle = stripos(strtolower($item['title']), strtolower($data['title'])) !== false;
+            }
+            if (!$filterTitle) return false;
+
+            // Subtitle
+            $filterSubtitle = !isset($data['subtitle']);
+            if (($data['subtitle'] ?? null)) {
+                $filterSubtitle = stripos(strtolower($item['subtitle']), strtolower($data['subtitle'])) !== false;
+            }
+            if (!$filterSubtitle) return false;
+
+            // Owner
+            $filterOwner = !isset($data['owner']);
+            if (($data['owner'] ?? null) && ($item['owner'] ?? null)) {
+                $filterOwner = $item['owner'] == $data['owner'];
+            }
+            if (!$filterOwner) return false;
+
+            // Assignees
+            $filterAssignees = !isset($data['assignees']);
+            if (($data['assignees'] ?? null) && ($item['assignees'] ?? null)) {
+                $filterAssignees = in_array($data['assignees'], $item['assignees']);
+            }
+            if (!$filterAssignees) return false;
+
+            // Tags
+            $filterTags = !isset($data['tags']);
+            if (($data['tags'] ?? null) && ($item['tags'] ?? null)) {
+                $tags = explode(',', strtolower($data['tags']));
+                $itemTags = array_map('strtolower', $item['tags']);
+                $filterTags = sizeof(array_intersect($tags, $itemTags)) > 0;
+            }
+            if (!$filterTags) return false;
+
+            return true;
+        })->toArray();
+    }
+
+    public function resetFilter(): void
+    {
+        $this->records = collect(KanbanService::getRecords())->toArray();
     }
 }
