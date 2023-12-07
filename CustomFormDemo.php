a2306afa0b37060b87068f04567ef651e34078fc
EL OUFIR Hatim <eloufirhatim@gmail.com>
2023-12-07 18:50:59 -0500

    Update CustomFormDemo.php

--- CustomFormDemo.php
+++ CustomFormDemo.php
@@ -4,7 +4,9 @@ namespace App\Filament\Pages;
 
 use App\Core\KanbanService;
 use Filament\Actions\Action;
+use Filament\Forms\Components\Checkbox;
 use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
 use Filament\Forms\Form;
 use Filament\Notifications\Notification;
 use Heloufir\FilamentKanban\Livewire\Kanban;
@@ -19,9 +21,11 @@ class CustomFormDemo extends Kanban
 
     public bool $showFilters = true;
 
-    protected static ?string $slug = 'custom-form-demo';
+    protected static ?string $slug = 'demo/custom-form';
 
-    protected static ?string $title = 'Custom Form Demo';
+    protected static ?string $title = 'Custom Form';
+
+    protected static ?string $navigationGroup = 'Demo';
 
     protected $listeners = [
         'filament-kanban.record-sorted' => 'recordSorted',
@@ -191,6 +195,10 @@ class CustomFormDemo extends Kanban
             TextInput::make('record.extra_2')
                 ->label('Extra input 2')
                 ->helperText('An extra input added by overriding the "form" method'),
+
+            Toggle::make('record.extra_3')
+                ->label('Extra input 3')
+                ->default(false)
         ]);
     }
 }
