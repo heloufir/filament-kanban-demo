4ceaaaef4ba30bd0c148767424f2462ffdbed878
EL OUFIR Hatim <eloufirhatim@gmail.com>
2023-12-11 15:13:28 -0500

    Update KanbanService.php

--- KanbanService.php
+++ KanbanService.php
@@ -15,6 +15,16 @@ class KanbanService
         ];
     }
 
+    public static function getStatusesWithUuid(): array
+    {
+        return [
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549e1", 'name' => 'Draft', 'color' => 'gray', 'draggable' => true],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549e2", 'name' => 'Submitted', 'color' => 'blue', 'draggable' => false],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549e3", 'name' => 'Changes requested', 'color' => 'orangered', 'draggable' => true],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549e4", 'name' => 'Published', 'color' => 'green', 'draggable' => true],
+        ];
+    }
+
     public static function getResources(): array
     {
         return [
@@ -23,6 +33,17 @@ class KanbanService
             ['id' => 3, 'name' => 'Morgan Blake'],
             ['id' => 4, 'name' => 'Jordan Frost', 'avatar' => 'https://ui-avatars.com/api/?background=f97316&color=fff7ed&size=200&name=Jordan Frost'],
             ['id' => 5, 'name' => 'Casey Quinn'],
+        ];
+    }
+
+    public static function getResourcesWithUuid(): array
+    {
+        return [
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549d1", 'name' => 'Alex Roe'],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549d2", 'name' => 'Taylor Nox', 'avatar' => 'https://robohash.org/4551ed2a34c28d96abdba7cbe1a97141?set=set4&bgset=bg2&size=200x200'],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549d3", 'name' => 'Morgan Blake'],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549d4", 'name' => 'Jordan Frost', 'avatar' => 'https://ui-avatars.com/api/?background=f97316&color=fff7ed&size=200&name=Jordan Frost'],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549d5", 'name' => 'Casey Quinn'],
         ];
     }
 
@@ -69,6 +90,29 @@ class KanbanService
             ['id' => 15, 'status' => 4, 'title' => 'Record 10 Col 4', 'subtitle' => 'filament-kanban #104', 'sort' => 9, 'draggable' => true, 'click' => true, 'delete' => true, 'owner' => 5, 'assignees' => [1, 2], 'tags' => ['web', 'laravel', 'filament', 'kanban'], 'extra_1' => 'Extra value 1', 'extra_2' => 'Extra value 2', 'extra_3' => true],
             ['id' => 16, 'status' => 1, 'title' => 'Record 1 Col 1', 'subtitle' => 'filament-kanban #11', 'sort' => 0, 'draggable' => true, 'click' => true, 'delete' => true, 'progress' => 0, 'owner' => 1, 'assignees' => [3, 4, 5], 'deadline' => '2023-12-02', 'tags' => [], 'extra_1' => 'Extra value 1', 'extra_2' => 'Extra value 2', 'extra_3' => false],
             ['id' => 17, 'status' => 1, 'title' => 'Record 2 Col 1', 'subtitle' => 'filament-kanban #21', 'sort' => 1, 'draggable' => true, 'click' => true, 'delete' => true, 'progress' => 10, 'owner' => 2, 'assignees' => [6], 'deadline' => '2023-12-05', 'tags' => ['web'], 'extra_1' => 'Extra value 1', 'extra_2' => 'Extra value 2', 'extra_3' => true],
+        ];
+    }
+
+    public static function getRecordsWithUuid(): array
+    {
+        return [
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549f1", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e2", 'title' => 'Record 1 Col 2', 'subtitle' => 'filament-kanban #12', 'sort' => 0, 'draggable' => true, 'click' => true, 'delete' => false, 'progress' => 20, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d1", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d2", "6491f95e-4e10-45b0-aad3-24edca3549d3", "6491f95e-4e10-45b0-aad3-24edca3549d4", "6491f95e-4e10-45b0-aad3-24edca3549d5"], 'tags' => ['filament', 'kanban']],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549f2", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e3", 'title' => 'Record 1 Col 3', 'subtitle' => 'filament-kanban #13', 'sort' => 0, 'draggable' => true, 'click' => true, 'delete' => true, 'progress' => 30, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d2", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d2"], 'deadline' => '2023-12-07', 'tags' => ['laravel', 'filament', 'kanban']],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549f3", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e3", 'title' => 'Record 2 Col 3', 'subtitle' => 'filament-kanban #23', 'sort' => 1, 'draggable' => false, 'click' => true, 'delete' => true, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d3", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d3"]],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549f4", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e3", 'title' => 'Record 3 Col 3', 'subtitle' => 'filament-kanban #33', 'sort' => 2, 'draggable' => true, 'click' => false, 'delete' => true, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d4", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d3", "6491f95e-4e10-45b0-aad3-24edca3549d4"], 'deadline' => '2024-02-02', 'tags' => ['web', 'laravel', 'filament']],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549f5", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e3", 'title' => 'Record 4 Col 3', 'subtitle' => 'filament-kanban #43', 'sort' => 3, 'draggable' => true, 'click' => true, 'delete' => true, 'progress' => 35, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d5", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d5"], 'tags' => ['web', 'laravel']],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549f6", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e4", 'title' => 'Record 1 Col 4', 'subtitle' => 'filament-kanban #14', 'sort' => 0, 'draggable' => true, 'click' => true, 'delete' => true, 'progress' => 40, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d1", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d4"], 'tags' => ['web', 'laravel', 'kanban']],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549f7", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e4", 'title' => 'Record 2 Col 4', 'subtitle' => 'filament-kanban #24', 'sort' => 1, 'draggable' => true, 'click' => true, 'delete' => true, 'progress' => 50, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d2", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d4", "6491f95e-4e10-45b0-aad3-24edca3549d5"], 'deadline' => '2023-12-29', 'tags' => ['web']],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549f8", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e4", 'title' => 'Record 3 Col 4', 'subtitle' => 'filament-kanban #34', 'sort' => 2, 'draggable' => true, 'click' => true, 'delete' => true, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d3", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d2", "6491f95e-4e10-45b0-aad3-24edca3549d4"], 'tags' => []],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549f9", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e4", 'title' => 'Record 4 Col 4', 'subtitle' => 'filament-kanban #44', 'sort' => 3, 'draggable' => true, 'click' => true, 'delete' => true, 'progress' => 100, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d4", 'deadline' => '2023-11-15', 'tags' => []],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549g1", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e4", 'title' => 'Record 5 Col 4', 'subtitle' => 'filament-kanban #54', 'sort' => 4, 'draggable' => true, 'click' => true, 'delete' => true, 'progress' => 80, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d5", 'tags' => ['kanban']],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549g2", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e4", 'title' => 'Record 6 Col 4', 'subtitle' => 'filament-kanban #64', 'sort' => 5, 'draggable' => true, 'click' => true, 'delete' => true, 'progress' => 75, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d1", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d2"], 'tags' => ['laravel']],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549g3", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e4", 'title' => 'Record 7 Col 4', 'subtitle' => 'filament-kanban #74', 'sort' => 6, 'draggable' => true, 'click' => true, 'delete' => true, 'progress' => 90, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d3", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d2"], 'deadline' => '2023-11-02', 'tags' => ['kanban']],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549g4", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e4", 'title' => 'Record 8 Col 4', 'subtitle' => 'filament-kanban #84', 'sort' => 7, 'draggable' => true, 'click' => true, 'delete' => true, 'progress' => 85, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d4", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d3"], 'deadline' => '2023-10-27', 'tags' => ['web', 'laravel', 'filament', 'kanban']],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549g5", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e4", 'title' => 'Record 9 Col 4', 'subtitle' => 'filament-kanban #94', 'sort' => 8, 'draggable' => true, 'click' => true, 'delete' => true, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d4", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d1"], 'tags' => ['web']],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549g6", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e4", 'title' => 'Record 10 Col 4', 'subtitle' => 'filament-kanban #104', 'sort' => 9, 'draggable' => true, 'click' => true, 'delete' => true, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d5", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d1", "6491f95e-4e10-45b0-aad3-24edca3549d2"], 'tags' => ['web', 'laravel', 'filament', 'kanban']],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549g7", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e1", 'title' => 'Record 1 Col 1', 'subtitle' => 'filament-kanban #11', 'sort' => 0, 'draggable' => true, 'click' => true, 'delete' => true, 'progress' => 0, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d1", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d3", "6491f95e-4e10-45b0-aad3-24edca3549d4", "6491f95e-4e10-45b0-aad3-24edca3549d5"], 'deadline' => '2023-12-02', 'tags' => []],
+            ['id' => "6491f95e-4e10-45b0-aad3-24edca3549g8", 'status' => "6491f95e-4e10-45b0-aad3-24edca3549e1", 'title' => 'Record 2 Col 1', 'subtitle' => 'filament-kanban #21', 'sort' => 1, 'draggable' => true, 'click' => true, 'delete' => true, 'progress' => 10, 'owner' => "6491f95e-4e10-45b0-aad3-24edca3549d2", 'assignees' => ["6491f95e-4e10-45b0-aad3-24edca3549d6"], 'deadline' => '2023-12-05', 'tags' => ['web']],
         ];
     }
 
